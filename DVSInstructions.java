9949f8c29cb7cab074b192ef62113519231ad5ba
Francis Obiagwu <fco25@drexel.edu>
2018-05-23 13:20:37 -0400

    This class is used to retrieve common instructions for the user

---
+++ DVSInstructions.java
@@ -0,0 +1,24 @@
+/**
+ * DVSInstructions
+ * This class is used to retrieve common instructions for the user
+ * @author Francis Obiagwu
+ * @version 1
+ * @date 5/23/2018
+ */
+public class DVSInstructions {
+    String openingInstruction = "Welcome to Dataset Visualization Software\n" +
+            "-----------------------------------------\n" +
+            "To select option, type in the option #\n" +
+            "1. Sign in\n" +
+            "2. Sign up\n" +
+            "3. Reset password or retrieve username\n" +
+            "4. Quit";
+    
+    String visualizationInstruction = "Select visualization option\n" +
+            "1. Notepad\n" +
+            "2. Excel\n" +
+            "3. Graph\n" +
+            "4. Return to the main menu\n";
+
+
+}
