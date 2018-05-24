697003ea9eefd17fe400d998690187a1439f2cfa
Justin Buckley <justin.m.buckley@gmail.com>
2018-05-24 14:35:11 -0400

    Sample and value up.

---
+++ src/main/java/edu/drexel/se577/grouptwo/viz/dataset/Sample.java
@@ -0,0 +1,20 @@
+package edu.drexel.se577.grouptwo.viz.dataset;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class Sample {
+    private final Value.Mapping mapping = new Value.Mapping();
+
+    public void put(String name, Value value) {
+        mapping.put(name, value);
+    }
+
+    public Optional<? extends Value> get(String name) {
+        return mapping.get(name);
+    }
+
+    public Collection<String> getKeys() {
+        return mapping.getKeys();
+    }
+}
